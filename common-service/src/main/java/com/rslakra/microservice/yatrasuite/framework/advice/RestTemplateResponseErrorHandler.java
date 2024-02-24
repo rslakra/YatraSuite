4ca69e2d3b47dafbaf4bef6da8b5d457784f03b7
Rohtash Lakra <rslakra@users.noreply.github.com>
2024-02-24 13:39:34 -0800

    merged local changes and updated appsuite-core version

--- common-service/src/main/java/com/rslakra/microservice/yatrasuite/framework/advice/RestTemplateResponseErrorHandler.java
+++ common-service/src/main/java/com/rslakra/microservice/yatrasuite/framework/advice/RestTemplateResponseErrorHandler.java
@@ -33,13 +33,13 @@ public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
     public void handleError(ClientHttpResponse httpResponse) throws IOException {
         if (httpResponse.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR) {
             // handle SERVER_ERROR
-//            throw new HttpClientErrorException(httpResponse.getStatusCode());
+            // throw new HttpClientErrorException(httpResponse.getStatusCode());
             throw new HttpServerErrorException(httpResponse.getStatusCode(), httpResponse.getStatusText());
         } else if (httpResponse.getStatusCode().series() == HttpStatus.Series.CLIENT_ERROR) {
             // handle CLIENT_ERROR
-//            if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-//                throw new RuntimeException(httpResponse.getStatusText());
-//            }
+            // if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+            //                throw new RuntimeException(httpResponse.getStatusText());
+            // }
             throw new HttpClientErrorException(httpResponse.getStatusCode(), httpResponse.getStatusText());
         }
     }
