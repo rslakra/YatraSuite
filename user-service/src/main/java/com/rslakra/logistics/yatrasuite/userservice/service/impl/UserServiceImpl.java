2637f179eab8f4b2247845e195bcf17e2ac5ff21
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-12-14 13:18:54 -0800

    update appsuite-spring v0.0.32

--- user-service/src/main/java/com/rslakra/logistics/yatrasuite/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/rslakra/logistics/yatrasuite/userservice/service/impl/UserServiceImpl.java
@@ -32,7 +32,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
--- vehicle-service/src/main/java/com/rslakra/logistics/yatrasuite/vehicleservice/persistence/entity/LocationHistory.java
+++ vehicle-service/src/main/java/com/rslakra/logistics/yatrasuite/vehicleservice/persistence/entity/LocationHistory.java
@@ -6,7 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import java.math.BigDecimal;
 
 /**
